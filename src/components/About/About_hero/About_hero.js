f4982970ee6c92c9049c041b81dfc997b189761a
avazbekowna <111364660+avazbekowna@users.noreply.github.com>
2023-02-20 10:36:05 +0600

    effee (#6)

--- src/components/About/About_hero/About_hero.js
+++ src/components/About/About_hero/About_hero.js
@@ -2,35 +2,21 @@ import React from 'react';
 import about1 from "../../../img/about1.png"
 
 const AboutHero = () => {
-    return (
-        <>
-            <div id="about">
-                <div className="container">
-                    <div className="best">
-                        <div className="about">
-                            <div className="about--us">
-                                <div className="about--us--us1"></div>
-                                <div className="about--us--person">
-                                    <img src={about1} alt=""/>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="education">
-                            <span>About Us</span>
-                            <h1>Best Education
-                                Platform</h1>
-                            <h2>Apparently we had reached a great height in the atmosphere, for the sky was a dead black.</h2>
-                            <h3>By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle.</h3>
-                        </div>
-
-                    </div>
-
+    return (<div id="about-hero">
+        <div className="container">
+            <div className="about-hero">
+                <div className="about-hero_bg"><img src={about1} alt=""/>
                 </div>
-
+                <div className="about-hero_desc">
+                    <span className="about-hero_desc-subtitle">About Us</span>                        <h1
+                    className="about-hero_desc-title">Best Education Platform</h1>
+                    <span className="about-hero_desc-bold">Apparently we had reached a great height in the atmosphere, for the sky was a dead black.</span>
+                    <p>By the same illusion which lifts the horizon of the sea to the level of the spectator on a
+                        hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle.</p>
+                </div>
+                <div className="about-hero_frame"></div>
             </div>
-        </>
-    );
+        </div>
+    </div>);
 };
-
 export default AboutHero;
--- src/components/About/About_team/About_team.js
+++ src/components/About/About_team/About_team.js
@@ -1,70 +1,52 @@
+
+
+
+import React from 'react';
 import about2 from "./../../../img/about3.png"
 import about3 from "./../../../img/about4.png"
 import about4 from "./../../../img/about5.png"
 import about5 from "./../../../img/about6.png"
 import about6 from "./../../../img/about7.png"
 import about7 from "./../../../img/about8.png"
-
 const AboutTeam = () => {
     return (
-        <>
-            <div id="teams">
-                <div className="container">
-                    <h1 className="teams--title">Our Team</h1>
-                    <div className="team">
-                        <div className="team--block">
-                           <div className="images">
-                               <img src={about2} alt=""/>
-                           </div>
-                            <h1>Miss.Anushin oont </h1>
-                            <p>CEO & co-Founder</p>
-                        </div>
-                        <div className="team--block">
-                           <div className="images">
-                               <img src={about3} alt=""/>
-                           </div>
-                            <h1>Miss.Anushin oont </h1>
-                            <p>CEO & co-Founder</p>
-                        </div>
-                        <div className="team--block">
-                           <div className="images">
-                               <img src={about4} alt=""/>
-                           </div>
-                            <h1>Miss.Anushin oont </h1>
-                            <p>CEO & co-Founder</p>
-                        </div>
-
-                        <div className="team--block">
-                           <div className="images">
-                               <img src={about5} alt=""/>
-                           </div>
-                            <h1>Miss.Anushin oont </h1>
-                            <p>CEO & co-Founder</p>
-                        </div>
-
-                        <div className="team--block">
-                          <div className="images">
-                              <img src={about6} alt=""/>
-                          </div>
-                            <h1>Miss.Anushin oont </h1>
-                            <p>CEO & co-Founder</p>
-                        </div>
-
-                        <div className="team--block">
-                            <div className="images">
-                                <img src={about7} alt=""/>
+        <div id="our-team">
+            <div className="container">
+                <div className="our-team"><h1 className="our-team_title">Our Team</h1>
+                    <div className="our-team_blocks">
+                        <div className="our-team_blocks-block">
+                            <div className="our-team_blocks-block_bg"><img src={about2} alt=""/>
                             </div>
-                            <h1>Miss.Anushin oont </h1>
-                            <p>CEO & co-Founder</p>
+                            <p>Miss.Anushin oont </p>
+                            <span>CEO & co-Founder</span></div>
+                        <div className="our-team_blocks-block">
+                            <div className="our-team_blocks-block_bg">
+                                <img src={about3} alt=""/></div>
+                            <p>mr.Vishmas s. </p>                            <span>CTO & co-Founder</span>
+                        </div>
+                        <div className="our-team_blocks-block">
+                            <div className="our-team_blocks-block_bg"><img src={about4} alt=""/>
+                            </div>
+                            <p>Miss.Tanisha kaduu </p>
+                            <span>VP Of Technology</span></div>
+                        <div className="our-team_blocks-block">
+                            <div className="our-team_blocks-block_bg">
+                                <img src={about5} alt=""/></div>
+                            <p>Mr.KL Rahul </p>                            <span>Head of design</span>
+                        </div>
+                        <div className="our-team_blocks-block">
+                            <div className="our-team_blocks-block_bg"><img src={about6} alt=""/>
+                            </div>
+                            <p>Miss.vashika g. </p>
+                            <span>vP Of Finance</span></div>
+                        <div className="our-team_blocks-block">
+                            <div className="our-team_blocks-block_bg">
+                                <img src={about7} alt=""/></div>
+                            <p>Mr.Aryan Singh </p>                            <span>Head of Marketing</span>
                         </div>
                     </div>
-
-
                 </div>
-
             </div>
-        </>
-    );
+        </div>);
 };
-
 export default AboutTeam;
